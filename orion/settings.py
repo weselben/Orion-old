9439b8e6d0d5da8affdc2625f80fd2c9f63a7bd3
Ben <50115212+weselben@users.noreply.github.com>
2023-03-17 16:34:28 +0100

    fix: secretkey leak

--- orion/settings.py
+++ orion/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qyb&s7k5-mw26j=dzp)httay)qk0ag0=qgsuav8z_019a*^uou'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
